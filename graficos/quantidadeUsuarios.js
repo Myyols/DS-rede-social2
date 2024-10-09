8a39ecf4733d57261809ce45a263844d43b172b7
Myyols <126932534+Myyols@users.noreply.github.com>
2024-10-09 08:15:12 -0300

    dia09

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,8 +22,27 @@ Plotly.newPlot(grafico, data, layout)
 
 
     }
+// código omitido
+
 const layout = {
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0,
+     font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
+     }
+  },
+    xaxis: {
+    
+    },
+    yaxis: {
+    
+    }
 }
+
+// código omitido
 quantidadeUsuarios()
