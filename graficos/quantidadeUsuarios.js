30fd1d45bce3005d247fb0a686c641ab0cd14cab
Myyols <126932534+Myyols@users.noreply.github.com>
2024-10-16 07:55:03 -0300

    dia16

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -18,9 +18,26 @@ async function quantidadeUsuarios() {
     }
   ]
 
+
   const layout = {
     plot_bgcolor: getCSS('--bg-color'),
     paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    },
+      xaxis: {
+      
+      },
+      yaxis: {
+      
+      }
+  }
        
   }
 
@@ -31,6 +48,6 @@ async function quantidadeUsuarios() {
 
 
 
-}
+
 
 quantidadeUsuarios()
