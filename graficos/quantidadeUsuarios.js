ef0d5cdaa22c610492b4e3c80443733b5ca7c9dd
Myyols <126932534+Myyols@users.noreply.github.com>
2024-10-16 07:45:45 -0300

    dia16

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -20,13 +20,16 @@ async function quantidadeUsuarios() {
 
   const layout = {
     plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
+    paper_bgcolor: getCSS('--bg-color'),
+       
   }
 
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
   Plotly.newPlot(grafico, data, layout)
+
+
 
 }
 
